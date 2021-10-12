02ee1e39b9da632887569d41aa9e834e7843c81f
Re van Zyl <re@zenaptix.com>
2021-10-12 14:20:12 +0200

    retry

--- src/main/resources/spark_utils.py
+++ src/main/resources/spark_utils.py
@@ -1,3 +1,2 @@
-class UtilWrapper:
-    def listFiles(filepath):
-        return sc._jvm.com.github.whaitukay.utils.UtilWrapper.listFiles(filepath)
+def listFiles(filepath):
+    return sc._jvm.com.github.whaitukay.utils.UtilWrapper.listFiles(filepath)
