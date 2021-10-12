91f5cc7613a0e0e86548aa291204a4a12a944f3c
Re van Zyl <re@zenaptix.com>
2021-10-12 19:19:40 +0200

    Update after review

--- src/main/resources/spark_utils.py
+++ src/main/resources/spark_utils.py
@@ -1,8 +1,8 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.getOrCreate()
-sc=spark.sparkContext
+spark = pyspark.sql.SparkSession._instantiatedSession
+sc = spark.sparkContext
 
 def listFiles(filepath):
     return sc._jvm.com.github.whaitukay.utils.UtilWrapper.listFiles(filepath)
